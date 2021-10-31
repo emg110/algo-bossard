a05ea1b0fdd713d4607acfe5a57a7c1b539bd995
Mohammad Ghiasi <emg110@gmail.com>
2021-10-31 12:43:45 +0330

    - updated app source

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,6 +49,7 @@ return
 `;
 const appProg = `
 #pragma version 5
+
 txn OnCompletion
 int NoOp
 ==
@@ -74,69 +75,57 @@ int DeleteApplication
 ==
 bnz handle_deleteapp
 
-// Unexpected OnCompletion value. Should be unreachable.
 err
 
 handle_noop:
-// Handle NoOp
-// Check for creator
-addr AMESZ5UX7ZJL5M6GYEHXM63OMFCPOJ23UXCQ6CVTI2HVX6WUELYIY262WI
-txn Sender
-==
-bnz handle_optin
 
-// read global state
-byte "counter"
+
+byte "bsbqty"
 dup
 app_global_get
-
-// increment the value
-int 1
+txna ApplicationArgs 1
 +
 
-// store to scratch space
 dup
 store 0
 
-// update global state
 app_global_put
 
-// read local state for sender
+
 int 0
-byte "counter"
+byte "bsbqty"
 app_local_get
 
-// increment the value
-int 1
+
+txna ApplicationArgs 2
 +
 store 1
 
-// update local state for sender
-// update "counter"
+
 int 0
-byte "counter"
+byte "bsbqty"
 load 1
 app_local_put
 
-// update "timestamp"
+
 int 0
 byte "timestamp"
-txn ApplicationArgs 0
+txn ApplicationArgs 3
 app_local_put
 
-// load return value as approval
+
 load 0
 return
 
 handle_optin:
-// Handle OptIn
-// approval
+int 0
+byte "bsbqty"
+int 0
+app_local_put
 int 1
 return
 
 handle_closeout:
-// Handle CloseOut
-//approval
 int 1
 return
 
@@ -148,7 +137,7 @@ txn Sender
 return
 
 handle_updateapp:
-// Check for creator
+
 addr AMESZ5UX7ZJL5M6GYEHXM63OMFCPOJ23UXCQ6CVTI2HVX6WUELYIY262WI
 txn Sender
 ==
